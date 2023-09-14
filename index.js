af6e87fe21d31c95ffbfb6117196b9fa708b6ae5
Luna <imlvnaa@gmail.com>
2023-09-14 09:48:55 -0400

    [skip ci] healthcheck

--- index.js
+++ index.js
@@ -5,38 +5,53 @@ import sharp from "sharp";
 
 const port = 19092;
 
-const allowedOutFormats = new Set(["jpeg", "png", "webp", "gif", "avif", "tiff"]);
+const allowedOutFormats = new Set([
+  "jpeg",
+  "png",
+  "webp",
+  "gif",
+  "avif",
+  "tiff",
+]);
 
 const app = express();
 
 app.get("/", async (req, res) => {
-	try {
-		if (!req.query.filepath || !req.query.format) return res.status(400).send("Bad Request");
+  try {
+    if (!req.query.filepath || !req.query.format)
+      return res.status(400).send("Bad Request");
 
-		const filePath = req.query.filepath,
-			format = req.query.format;
+    const filePath = req.query.filepath,
+      format = req.query.format;
 
-		if (!isAbsolute(filePath) || !existsSync(filePath) || !allowedOutFormats.has(format)) {
-			return res.status(400).send("Bad Request");
-		}
+    if (
+      !isAbsolute(filePath) ||
+      !existsSync(filePath) ||
+      !allowedOutFormats.has(format)
+    ) {
+      return res.status(400).send("Bad Request");
+    }
 
-		const fileParts = filePath.split(sep),
-			fileDir = fileParts.slice(0, fileParts.length - 1).join(sep),
-			fileName = fileParts[fileParts.length - 1],
-			fileNoExt = fileName.split(".")[0];
+    const fileParts = filePath.split(sep),
+      fileDir = fileParts.slice(0, fileParts.length - 1).join(sep),
+      fileName = fileParts[fileParts.length - 1],
+      fileNoExt = fileName.split(".")[0];
 
-		if (existsSync(join(fileDir, `${fileNoExt}.${format}`))) return res.status(200).send("OK");
+    if (existsSync(join(fileDir, `${fileNoExt}.${format}`)))
+      return res.status(200).send("OK");
 
-		const file = readFileSync(filePath);
-		await sharp(file)
-			.toFormat(format)
-			.toFile(join(fileDir, `${fileNoExt}.${format}`));
+    const file = readFileSync(filePath);
+    await sharp(file)
+      .toFormat(format)
+      .toFile(join(fileDir, `${fileNoExt}.${format}`));
 
-		return res.status(200).send("OK");
-	} catch (err) {
-		console.error(err);
-		return res.status(500).send("Internal Server Error");
-	}
+    return res.status(200).send("OK");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
 });
+
+app.get("/health", (req, res) => res.status(200).send("OK"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
